b10d5967217518f84b615f765b8f5eba90d42943
Nikhil-Satish <satishnikhil307@gmail.com>
2024-03-09 04:17:20 +0530

    Updated Interface

--- src/TestInterface.js
+++ src/TestInterface.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { questions_list } from "./Questions";
 import { useState } from "react";
+import './design/TestInterface.css'
 
 function TestInterface(){
     const [current_q, set_q] = useState(0);
+    const anslist = []
+    for(var i = 0;i<questions_list.length;i++){
+        anslist.push(5);
+    }
+    const [answers_entered, setAnswers] = useState(anslist);
+    const changeAnswers = () =>{
+        const newAnswer = answers_entered.map((c, i) => {
+            if (i === current_q) {
+              return selected_value;
+            } else {
+              return c;
+            }
+          });
+          setAnswers(newAnswer);
+    }
     const [selected_value, setSelected] = useState(5);
     const handleNextClick = () =>{
         setSelected(5);
@@ -28,31 +44,39 @@ function TestInterface(){
 
     return(
         <div className="test-interface">
-            <div className="prev-button">
-                <button onClick={handlePrevClick} >Previous</button>
+            <div className="quest">
+                <div>
+                    <p>{questions_list[current_q].question}</p>
+                    <ul className="options">
+                        {questions_list[current_q].options.map((option, optionIndex) => (
+                        <li key={optionIndex}>
+                            <label>
+                            <input
+                                type="radio"
+                                id={optionIndex} 
+                                value={option} 
+                                checked={selected_value === optionIndex}
+                                onChange={()=>handleRadioChange(optionIndex)}
+                            />
+                            {option}
+                            </label>
+                        </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="prev-button">
+                    <button onClick={handlePrevClick} >Previous</button>
+                </div>
+                <div className="next-button">
+                    <button onClick={handleNextClick} >Next</button>
+                </div>
+                
             </div>
-            <p>{questions_list[current_q].question}</p>
-            <ul className="options">
-                {questions_list[current_q].options.map((option, optionIndex) => (
-                <li key={optionIndex}>
-                    <label>
-                    <input
-                        type="radio"
-                        id={optionIndex} 
-                        value={option} 
-                        checked={selected_value === optionIndex}
-                        onChange={()=>handleRadioChange(optionIndex)}
-                     />
-                    {option}
-                    </label>
-                </li>
-                ))}
-            </ul>
-            <div className="next-button">
-                <button onClick={handleNextClick} >Next</button>
-            </div>
+            <br/>
+            <br/>
+
             <div className="submit-button">
-                <button onClick={handleSubmit} >Next</button>
+                <button onClick={handleSubmit} >Submit</button>
             </div>
         </div>
     )
--- src/Timer.js
+++ src/Timer.js
@@ -6,17 +6,21 @@ const Timer = () => (
   <CountdownCircleTimer
     isPlaying
     duration={70}
+    size={90}
     colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-    colorsTime={[7, 5, 2, 0]}
+    colorsTime={[300, 60, 15, 0]}
   >
   
     {/* {({ remainingTime }) => remainingTime} */}
     {({ remainingTime }) => {
         const hours = Math.floor(remainingTime / 3600)
+        let hh = String(hours).padStart(2,'0');
         const minutes = Math.floor((remainingTime % 3600) / 60)
+        let mm = String(minutes).padStart(2,'0');
         const seconds = remainingTime % 60
+        let sec = String(seconds).padStart(2,'0');
     
-        return `${hours}:${minutes}:${seconds}`
+        return `${hh}:${mm}:${sec}`
     }}
   </CountdownCircleTimer>
 )
