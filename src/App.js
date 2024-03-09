35fa3979e0cb6c3f4b3a57b906cbcb9215577e99
Nikhil-Satish <satishnikhil307@gmail.com>
2024-03-10 04:35:08 +0530

    Added router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import TestInterface from './TestInterface';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Uploader from './Uploader';
 
 function App() {
   return (
-      <div>
-        <h1 style={{textAlign: 'center', margin: 10}}>Online Testing Platform</h1>
-        <div className='main-area'>
-          <div className='inter' >
-            <TestInterface />
+      // <div>
+      //   <h1 style={{textAlign: 'center', margin: 10}}>Online Testing Platform</h1>
+      //   <div className='main-area'>
+      //     <div className='inter' >
+      //       <TestInterface />
+      //     </div>
+      //   </div>
+      // </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Uploader />}>
+        </Route>
+        <Route 
+        path='/test' element={
+          <div>
+            <h1 style={{textAlign: 'center', margin: 10}}>Online Testing Platform</h1>
+            <div className='main-area'>
+              <div className='inter' >
+                <TestInterface />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
