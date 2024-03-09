1412bcfd861f7e4b69fd2900b2791b89155f4dee
Adarsh Kishore <adarsh.kishore786@gmail.com>
2024-03-09 14:13:13 +0530

    Saving json produced to a file

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import subprocess
+import re
 import sys
+import json
 
 def capture_command_output(command):
     result = subprocess.run(command, capture_output=True, text=True, shell=True)
@@ -9,13 +11,49 @@ def capture_command_output(command):
 def trim_output(questions):
     new_questions = []
     for q in questions:
-        idx = q.index("\item")
-        tmp = q[idx+6:].strip("\\")
+        tmp = q.strip("\n").strip(r"\\")
         new_questions.append(tmp)
     return new_questions
 
-command = f"cat {sys.argv[1]} | awk \"/begin{{enumerate}}/,/end{{enumerate}}/\" | grep '\\item'"
-questions = trim_output(capture_command_output(command).split("\n"))
+def get_question_option_answer_from_text(file_url):
+    command = f"cat {file_url} | awk '/begin{{enumerate}}/,/Ans/'"
+    text = capture_command_output(command).split("\\begin{enumerate}")[1:]
+    # for i, q in enumerate(text):
+    #     print(f"{i}: {q}", end='\n')
+    return trim_output(text)
+    
+def build_question_paper(file_url):
+    question_paper = []
+    text = get_question_option_answer_from_text(file_url)
 
-for q in questions:
-    print(q, "\n")
+    for qa in text:
+        # print(qa)
+        question_option_answer = { 
+            "question": "",
+            "options": [],
+            "answer": qa.split("\n")[-1][5:].strip("(").strip(")")
+        }
+        for i, line in enumerate(re.split("\(\d\)\s", qa)):
+            tmp = line.strip().strip("\n").rstrip(r"\\")
+            tmp = tmp.replace(r"\end{enumerate}", "")
+
+            if i == 0:
+                tmp = tmp[tmp.index(r"\item"):].replace(r"\item ", "")
+                tmp = re.sub("Ans.*", "", tmp)
+                question_option_answer['question'] = tmp
+
+            if 1 <= i <= 4:
+                tmp = tmp.split("Ans")[0].strip("\n")
+                tmp = tmp.split(r"\end{enumerate}")[0].strip("\n")
+
+                # if not re.match("^\(\d\)", tmp):
+                #     continue
+
+                question_option_answer['options'].append(tmp)
+
+        question_paper.append(question_option_answer)
+
+    return json.dumps(question_paper, indent=3)
+
+paper = build_question_paper(sys.argv[1])
+print(paper)
