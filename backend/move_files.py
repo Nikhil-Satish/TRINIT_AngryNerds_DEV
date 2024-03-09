1e00de8a484b3047e87c716afe3982f0e5529a18
Adarsh Kishore <adarsh.kishore786@gmail.com>
2024-03-09 19:17:03 +0530

    Unzipping images as well now

--- backend/move_files.py
+++ backend/move_files.py
@@ -2,13 +2,19 @@ import os
 import subprocess
 import sys
 
-def detect_tex():
-    for dirname, _, filenames in os.walk('./zip'):
-        if len(filenames) == 0:
-            continue
+for dirname, _, filenames in os.walk('./zip'):
+    if len(filenames) == 0:
+        continue
 
-        if filenames[0].split('.')[-1] == "tex":
-            command = f"mv {dirname}/{filenames[0]} ./output/{sys.argv[1]}.tex"
-            subprocess.run(command, capture_output=True, text=True, shell=True)
+    if filenames[0].split('.')[-1] == "tex":
+        command = f"mv {dirname}/{filenames[0]} ./output/{sys.argv[1]}.tex"
+        subprocess.run(command, capture_output=True, text=True, shell=True)
 
-detect_tex()
+    if filenames[0].split('.')[-1] == "jpg":
+        for file_ in filenames:
+            file = file_.replace('(', '\(').replace(')', '\)')
+            print(f"{dirname}/{file}")
+            command = f"mv {dirname}/{file} ./src/images/"
+            print(command)
+            print(subprocess.run(command, capture_output=True, text=True, shell=True))
+            print()
