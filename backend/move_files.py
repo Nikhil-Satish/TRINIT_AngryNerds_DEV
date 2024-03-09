9caa33a491cc84e9b29f27c0896c4cde1b4ca27d
Adarsh Kishore <adarsh.kishore786@gmail.com>
2024-03-09 18:18:59 +0530

    Automating unzipping

---
+++ backend/move_files.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+import sys
+
+def detect_tex():
+    for dirname, _, filenames in os.walk('./output'):
+        if len(filenames) == 0:
+            continue
+
+        if filenames[0].split('.')[-1] == "tex":
+            command = f"mv {dirname}/{filenames[0]} ./output/{sys.argv[1]}.tex"
+            subprocess.run(command, capture_output=True, text=True, shell=True)
+
+detect_tex()
